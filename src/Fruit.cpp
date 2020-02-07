47c682011dc89bf29ef2648663e663ce8b1ee6e9
CiaranWelsh <cwelsh2@uw.edu>
2020-02-07 11:28:37 -0800

    First commit. Googletests is configured. About to import code from main repo

---
+++ src/Fruit.cpp
@@ -0,0 +1,11 @@
+//
+// Created by cwelsh on 2/7/2020.
+//
+
+#include <iostream>
+#include "Fruit.h"
+
+int Fruit::eat() {
+    std::cout << "eaty the fruity" << std::endl;
+    return 0;
+}
---
+++ tests/FruitTests.cpp
@@ -0,0 +1,23 @@
+//
+// Created by cwelsh on 2/7/2020.
+//
+
+#include "gtest/gtest.h"
+#include "Fruit.h"
+
+TEST(FruitTests, test){
+    Fruit f;
+    ASSERT_EQ(4, 5);
+}
+
+
+
+
+
+
+
+
+
+
+
+
